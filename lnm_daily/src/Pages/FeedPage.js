a804e1b9b85a19415871650e968101a173dfe908
Pranav Chatur <pranavchatur@gmail.com>
2022-09-02 23:56:32 +0530

    Ready feed page for clubs

--- lnm_daily/src/Pages/FeedPage.js
+++ lnm_daily/src/Pages/FeedPage.js
@@ -7,32 +7,29 @@ const FeedPage = () => {
   const images = [
     {
       label: "San Francisco – Oakland Bay Bridge, United States",
-      imgPath:
-        "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+      imgPath: "https://images.unsplash.com/photo-1537944434965-cf4679d1a598",
     },
     {
       label: "Bird",
-      imgPath:
-        "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+      imgPath: "https://images.unsplash.com/photo-1538032746644-0212e812a9e7",
     },
     {
       label: "Bali, Indonesia",
-      imgPath:
-        "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+      imgPath: "https://images.unsplash.com/photo-1537996194471-e657df975ab4",
     },
     {
       label: "Goč, Serbia",
-      imgPath:
-        "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+      imgPath:"https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=800&h=500&q=60",
     },
   ];
 
   return (
-    <div>
-      <div className="parent_feedpage">
+    <div className="parent_feedpage">
+      <div className="carousel_n_clubs">
         <div className="carousel">
-          <Carousel imageList={images} customWidth={800} />
-        </div>
+          <Carousel imageList={images} customWidth={800}/>
+          </div>
+        <div className="temp"></div>
       </div>
       <EventCard
         imagePath={
