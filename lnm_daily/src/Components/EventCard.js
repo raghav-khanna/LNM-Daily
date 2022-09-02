df80c2e348a83830323ee7aac2004e71b345594d
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-09-02 22:27:00 +0530

    Add Card, PopUp and Initial Styling

---
+++ lnm_daily/src/Components/EventCard.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import PopUp from "./PopUp";
+
+const EventCard = ({ eventName, briefDetail, fullDetails, imagePath }) => {
+  const [show, setShow] = React.useState(false);
+
+  const showPopUp = () => {
+    // <PopUp
+    //   EventName={eventName}
+    //   EventDetails={fullDetails}
+    //   isRegister={true}
+    // />
+    setShow(true);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="Required Image"
+        height="140"
+        image={imagePath}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {eventName}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {briefDetail}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {/* <Button size="small" onClick={showPopUp}>
+          
+        </Button> */}
+        <PopUp
+          EventName={eventName}
+          EventDetails={fullDetails}
+          isRegister={true}
+        />
+      </CardActions>
+    </Card>
+  );
+};
+
+export default EventCard;
--- lnm_daily/src/Pages/FeedPage.js
+++ lnm_daily/src/Pages/FeedPage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Carousel from "../Components/Carousel";
+import EventCard from "../Components/EventCard";
+import "./FeedPage.css";
 
 const FeedPage = () => {
   const images = [
@@ -27,7 +29,23 @@ const FeedPage = () => {
 
   return (
     <div>
-      <Carousel imageList={images} customWidth={800} />
+      <div className="parent_feedpage">
+        <div className="carousel">
+          <Carousel imageList={images} customWidth={800} />
+        </div>
+      </div>
+      <EventCard
+        imagePath={
+          "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60"
+        }
+        briefDetail={
+          "This is just brief detail of the event and what is it about and when it will happend"
+        }
+        fullDetails={
+          "This is just brief detail of the event and what is it about and when it will happend. This is now full detail of the event and hence it can also be registered into. Please click on the Register button below to register for the event"
+        }
+        eventName={"Event Name"}
+      />
     </div>
   );
 };
