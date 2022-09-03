6827d91a1c3f337a3c1d028fde7e108f74ab01aa
Pranav Chatur <pranavchatur@gmail.com>
2022-09-03 21:27:26 +0530

    Add Create Post page

--- lnm_daily/src/App.js
+++ lnm_daily/src/App.js
@@ -2,6 +2,8 @@ import FeedPage from "./Pages/Feed Page/FeedPage";
 import Parent from "./Pages/Parent/Parent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Clubs from "./Pages/Clubs/Clubs";
+import CreatePost from "./Pages/Create Post/CreatePost";
+import Footer from "./Pages/Footer/Footer";
 
 function App() {
   return (
@@ -10,42 +12,11 @@ function App() {
         <Route path="/" element={<Parent />}>
           <Route index element={<FeedPage />}></Route>
           <Route path="clubs" element={<Clubs />}></Route>
+          <Route path="new_post" element={<CreatePost/>}></Route>
         </Route>
-        <Route
-          path="*"
-          element={
-            <div>
-              <h1>Error!</h1>Change URL to /
-            </div>
-          }
-        />
+        <Route path="*" element={<div><h1>Error!</h1>Change URL to /</div>}/>
       </Routes>
-
-      <div
-        style={{ backgroundColor: "#2196f3", borderTop: "0.25rem black solid" }}
-      >
-        <div>
-          <h4>
-            Rights Reserved ® <br />
-            ..
-            <br />
-            Created by
-          </h4>
-          Nirnay(@nirnaykorde)
-          <br />
-          Pranav(@pranavchatur)
-          <br />
-          Raghav(@raghavkhanna)
-          <br />
-          Sourav(@souravjan)
-          <br />
-          ..
-          <br />
-          <h4>Made using React, Material UI, CassandraDB, NodeJS</h4>
-          <hr />
-          <hr />
-        </div>
-      </div>
+      <Footer/>
     </BrowserRouter>
   );
 }
