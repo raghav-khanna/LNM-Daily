7c5cd51989a81b13789768eb1232769fe948e212
Pranav Chatur <pranavchatur@gmail.com>
2022-09-04 11:14:46 +0530

    Add Relevant Images

--- lnm_daily/src/Pages/Clubs/Clubs.js
+++ lnm_daily/src/Pages/Clubs/Clubs.js
@@ -1,14 +1,20 @@
 import React from "react";
 import AvatarList from "../../Components/Avatar List/AvatarList";
+import MediaCell from "../../Assets/MediaCell.png"
+import Astro from "../../Assets/Astro.jpg"
+import Cybros from "../../Assets/Cybros.jpg"
+import Alumini from "../../Assets/Alumini.png"
+import Quizzinga from "../../Assets/QUIZINGA.jpg"
+import Gymkhana from "../../Assets/Gymkhana.png"
 import "./Clubs.css"
 
 const Clubs = () => {
     return (
         <div className="parent_clubs">
-            <h1>Cultural</h1>
+            <h1>Clubs</h1>
             <div className="content_clubs">
-                <AvatarList club1={"https://images.unsplash.com/photo-1538032746644-0212e812a9e7"}/>
-                <AvatarList/>
+                <AvatarList club1={MediaCell} club2={Astro} club3={Cybros}/>
+                <AvatarList club1={Alumini} club2={Quizzinga} club3={Gymkhana}/>
             </div>
         </div>
     );
